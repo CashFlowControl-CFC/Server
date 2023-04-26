895dcd422125fef7669117831835983da9b1a75c
Witcher-MTM <phantomit228@gmail.com>
2023-04-26 15:01:53 +0300

    add cors

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser');
 const db = require('./db');
-
 const TransactionRoute = require('./routes/transaction.route');
 const CategoryRoute = require('./routes/category.route');
 const LoadRoute = require('./routes/onLoad.route');
 const AccountRoute = require('./routes/account.route');
+
+
 app.use(bodyParser.json());
+app.use(cors());
 
 app.use('/transaction',TransactionRoute);
 app.use('/category',CategoryRoute);
